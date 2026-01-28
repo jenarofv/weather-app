7ba34077b9183da4d22e5021efc252b174276aa2
jenaro fv <jen4r0@gmail.com>
2026-01-28 12:29:47 -0600

    remove template files and add my own

---
+++ README.md
@@ -0,0 +1,3 @@
+# Weather App
+
+Weather app for [the Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-weather-app) using APIs and asychronous code.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-import diceImage from "./dice1.jpg";
 import "./style.css";
-import { greeting } from "./greeting.js";
-
-const image = document.createElement("img");
-image.src = diceImage;
-image.width = "200";
-
-document.body.appendChild(image);
-
-console.log(greeting);
+import Homepage from "./homepage.js";
