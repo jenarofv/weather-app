1578d7cabc7237ee5c1ab023403b65765e6cb185
jenaro fv <jen4r0@gmail.com>
2026-01-28 17:42:57 -0600

    checkpoint: successfully return forecast call

--- src/homepage.js
+++ src/homepage.js
@@ -1,4 +1,56 @@
 class Homepage {
+  constructor(title) {
+    const body = document.querySelector("body");
+    const titleContainer = document.createElement("h1");
+    titleContainer.id = "title";
+    body.appendChild(titleContainer);
+    this.title = title;
+    const main = document.createElement("main");
+    const locationField = document.createElement("input");
+    locationField.id = "location-field";
+    const locationLabel = document.createElement("label");
+    locationLabel.htmlFor = "location-field";
+    locationLabel.innerText = "Search for a location:"
+    this.btn = document.createElement("button");
+    this.btn.innerText = "search";
+    main.appendChild(locationLabel);
+    main.appendChild(locationField);
+    main.appendChild(this.btn);
+    body.appendChild(main);
+    this.weather = document.createElement("div");
+    this.weather.classList.add("weather");
+    const linkToMap = document.createElement("p");
+    linkToMap.classList.add("link-to-map");
+    this.weather.appendChild(linkToMap);
+    for (let i = 0; i < 7; i++) {
+      const day = document.createElement("div");
+      const icon = document.createElement("div");
+      const desc = document.createElement("p");
+      const dayName = document.createElement("p");
+      dayName.classList.add("day-name");
+      desc.classList.add("description");
+      day.classList.add("day");
+      day.id = `day-${i}`;
+      day.appendChild(icon);
+      day.appendChild(dayName);
+      day.appendChild(desc);
+      icon.classList.add("weather-icon");
+      this.weather.appendChild(day);
+    }
+    main.appendChild(this.weather);
+  }
+
+  get title() {
+    return document.getElementById("title");
+  }
+
+  set title(newTitle) {
+    document.getElementById("title").innerText = newTitle;
+  }
+
+  get location() {
+    return document.getElementById("location-field").value;
+  }
 
 }
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,52 @@
 import "./style.css";
 import Homepage from "./homepage.js";
+
+const homepage = new Homepage("Weather App");
+homepage.btn.addEventListener("click", () => fetchForecast(homepage.location)); 
+
+async function fetchForecast(location, metric=true) {
+  const icons = {
+    snow : "❄️",
+    rain : "🌧️",
+    fog : "🌫️",
+    wind : "🌬️",
+    cloudy : "☁️",
+    "partly-cloudy-day" : "🌤️",
+    "partly-cloudy-night" : "☁️",
+    "clear-day" : "🌞",
+    "clear-night" : "🌌",
+  }
+  const dayNames = {
+    0: "Mon",
+    1: "Tue",
+    2: "Wed",
+    3: "Thu",
+    4: "Fri",
+    5: "Sat",
+    6: "Thu"
+  }
+  const API = '3UTAV24BZFVF42D9WQCK72EFK';
+  try {
+    let response ;
+    if (metric) {
+      response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${API}`);
+    } else {
+      response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${API}`);
+    }
+    const jsonData = await response.json();
+    for (let i = 0; i < 7; i++){
+      const day = document.getElementById(`day-${i}`)
+      const icon = document.querySelector(`#day-${i} .weather-icon`)
+      const description = document.querySelector(`#day-${i} .description`);
+      const dayName = document.querySelector(`#day-${i} .day-name`);
+      day.classList.add("nice-border");
+      dayName.innerText = dayNames[(new Date(jsonData.days[i].datetime)).getDay()];
+      description.innerText = jsonData.days[i].description;
+      icon.innerText = icons[jsonData.days[i].icon];
+    }
+    localStorage.setItem("forecast", JSON.stringify(jsonData));
+  } catch (e) {
+    alert("We could not find any location with that name");
+    console.log(e);
+  }
+}
